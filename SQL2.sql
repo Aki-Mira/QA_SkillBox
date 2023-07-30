28e15287ccbdf3e8dde7a599eb2d61147dfb132f
AkiMira <akimiratest@gmail.com>
2023-07-30 16:44:16 +0300

    SQL2

---
+++ SQL2.sql
@@ -0,0 +1,11 @@
+-- DECRIPTION:
+-- Академия SQL https://sql-academy.org/
+-- База данных авиаперелеты
+-- Задание 2
+-- Вывести названия всеx авиакомпаний
+-- Поля в результирующей таблице:
+-- name
+
+-- SOLUTION:
+SELECT name
+FROM Company;
